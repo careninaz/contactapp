1ca0009e8d26845a96bece91e3664f0369f3f051
careninaz <118865034+careninaz@users.noreply.github.com>
2024-05-03 19:36:50 +0700

    routes.php

--- routes.php
+++ routes.php
@@ -10,16 +10,14 @@ Router::url('dashboard', 'get', 'DashboardController::index');
 Router::url('dashboard/admin', 'get', 'DashboardController::admin');
 Router::url('dashboard/contacts', 'get', 'DashboardController::contacts');
 Router::url('dashboard/logout', 'get', 'AuthController::logout');
-Router::url('contacts/add', 'get', 'ContactController::add');
-Router::url('contacts/edit', 'get', 'ContactController::edit');
-Router::url('contacts/remove', 'get', 'ContactController::remove');
-Router::url('freshdb', 'get', 'freshdb');
-Router::url('report', 'get', 'ContactController::report');
+Router::url('contacts/tambah', 'get', 'ContactController::add');
+Router::url('contacts/ubah', 'get', 'ContactController::edit');
+Router::url('contacts/hapus', 'get', 'ContactController::remove');
 
 
 Router::url('login', 'post', 'AuthController::saveLogin');
 Router::url('register', 'post', 'AuthController::saveRegister');
-Router::url('contacts/add', 'post', 'ContactController::saveAdd');
-Router::url('contacts/edit', 'post', 'ContactController::saveEdit');
+Router::url('contacts/tambah', 'post', 'ContactController::saveAdd');
+Router::url('contacts/ubah', 'post', 'ContactController::saveEdit');
 
 new Router();
